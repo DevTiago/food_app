ec6a6187b24d27d5e3d5a2a74627d951b684ff79
Tiago Oliveira <tiago.oliveira@upgrade.pt>
2021-02-11 17:57:59 +0000

    slides

--- lib/components/carousel.dart
+++ lib/components/carousel.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
-
 class Carousel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
--- lib/initial/initial_screen.dart
+++ lib/initial/initial_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:food_app/components/carousel.dart';
+import 'package:http/http.dart';
 
 class InitialScreen extends StatefulWidget {
   @override
@@ -7,6 +10,51 @@ class InitialScreen extends StatefulWidget {
 }
 
 class _InitialScreenState extends State<InitialScreen> {
+
+  var slides = new List();
+  List<dynamic> restaurantCategories;
+
+
+
+    // Future getSlides() async {
+    //   String url = "https://sandbox.pedecome.pt/api/promo-slider";
+    //   Map<String, String> headers = {"Content-type": "application/json"};
+    //   try {
+    //     Response response = await post(url, headers: headers);
+    //     Map data = jsonDecode(response.body);
+    //     data['mainSlides'].forEach((slide) => slides.add(slide['image_placeholder']));
+    //   } catch (e) {
+    //     print(e);
+    //   }
+    // }
+
+    Future getRestaurantCategories() async {
+      String url = 'https://sandbox.pedecome.pt/api/get-restaurant-categories';
+      Map<String, String> headers = {"Content-type": "application/json"};
+
+      try {
+        Response response = await post(url, headers: headers);
+        List<dynamic> data = jsonDecode(response.body);
+
+        setState(() => restaurantCategories = data);
+
+        print(restaurantCategories);
+
+
+      } catch (e) {
+        print(e);
+      }
+    }
+
+
+@override
+  // ignore: must_call_super
+  void initState() {
+    // getSlides();
+    getRestaurantCategories();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
